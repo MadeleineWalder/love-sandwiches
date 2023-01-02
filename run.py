1845ff127cd377cdd3dc53c3e150e883e68b7aba
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-01-02 11:23:49 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,21 +20,24 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
-    
+        data_str = input("Enter your data here:\n")
+        
         sales_data = data_str.split(",")
         
         if validate_data(sales_data):
             print("Data is valid!")
             break
 
-    return sales_data  
+    return sales_data
 
 
 def validate_data(values):
@@ -54,7 +57,7 @@ def validate_data(values):
         return False
     
 
-    return True 
+    return True
 
 
 # Old functions that have been refactored into the update_worksheet(data)
@@ -138,7 +141,7 @@ def calculate_stock_data(data):
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
         new_stock_data.append(round(stock_num))
-    
+       
     return new_stock_data
 
 
